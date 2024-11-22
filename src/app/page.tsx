faf4589b866c0c7c9bf6a8828dae24e7c868bbdb
AizazNaeem4433 <139700180+AizazNaeem4433@users.noreply.github.com>
2024-11-23 04:51:01 +0500

    Hero-section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import { Hero } from "./components/Hero";
-import { SectionTwo } from "./components/Sectiontwo";
-
+import { SectionTwo } from "./components/SecondSection"
 export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
